a920d92d1554d6b140a554e0300302dda45349b4
arima0714 <a1710021@edu.cc.uec.ac.jp>
2019-10-08 11:57:25 +0900

    2Dに関する簡易チュートリアルを終えた

---
+++ prog1/prog1.c
@@ -0,0 +1,40 @@
+#include <GL/glut.h>
+
+void display(void){
+	glClear(GL_COLOR_BUFFER_BIT);
+	glBegin(GL_POLYGON);
+	glColor3d(1.0, 0.0, 0.0);
+	glVertex2d(-0.9, -0.9);
+	glColor3d(0.0, 1.0, 0.0);
+	glVertex2d(0.9, -0.9);
+	glColor3d(1.0, 0.0, 1.0);
+	glVertex2d(0.9, 0.9);
+	glColor3d(1.0, 1.0, 0.0);
+	glVertex2d(-0.9, 0.9);
+	glEnd();
+	glFlush();
+}
+
+void resize(int w, int h){
+	glViewport(0,0,w,h);	//ウィンドウ全体をビューポートにする
+	glLoadIdentity();	//変換行列の初期化
+	glOrtho(-w / 200.0, w / 200.0, -h /200.0, h / 200.0, -1.0, 1.0);	//スクリーン上の表示領域をビューポートの大きさに比例させる
+}
+
+void init(void){
+	glClearColor(1.0, 1.0, 1.0, 1.0);
+}
+
+int main(int argc, char *argv[]){
+	glutInitWindowPosition(100,100);	//300x300のウィンドウを開く
+	glutInitWindowSize(320, 240);		//320,240の位置にウィンドウを開く
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_RGBA);
+	glutCreateWindow(argv[0]);
+	glutDisplayFunc(display);
+	glutReshapeFunc(resize);
+	init();
+	glutMainLoop();
+	return 0;
+}
+
