d10b61c7d98e89b491c7b5c729a8a84b9412237e
arima0714 <a1710021@edu.cc.uec.ac.jp>
2019-10-15 11:19:09 +0900

    絵をかけるようにした

--- prog2/prog1.c
+++ prog2/prog1.c
@@ -2,23 +2,42 @@
 
 void display(void){
 	glClear(GL_COLOR_BUFFER_BIT);
-	glBegin(GL_POLYGON);
-	glColor3d(1.0, 0.0, 0.0);
-	glVertex2d(-0.9, -0.9);
-	glColor3d(0.0, 1.0, 0.0);
-	glVertex2d(0.9, -0.9);
-	glColor3d(1.0, 0.0, 1.0);
-	glVertex2d(0.9, 0.9);
-	glColor3d(1.0, 1.0, 0.0);
-	glVertex2d(-0.9, 0.9);
-	glEnd();
 	glFlush();
 }
 
 void resize(int w, int h){
 	glViewport(0,0,w,h);	//ウィンドウ全体をビューポートにする
 	glLoadIdentity();	//変換行列の初期化
-	glOrtho(-w / 200.0, w / 200.0, -h /200.0, h / 200.0, -1.0, 1.0);	//スクリーン上の表示領域をビューポートの大きさに比例させる
+	glOrtho(-0.5, (GLdouble)w - 0.5, (GLdouble)h - 0.5, -0.5, -1.0, 1.0);
+}
+
+void mouse(int button, int state, int x, int y){
+	static int x0, y0;
+
+	switch(button){
+		case GLUT_LEFT_BUTTON:
+			if(state == GLUT_UP){
+				glColor3d(0.0, 0.0, 0.0);
+				glBegin(GL_LINES);
+				glVertex2i(x0, y0);
+				glVertex2i(x , y );
+				glEnd();
+				glFlush();
+			}
+			else{
+				// ボタンを押した位置を覚える
+				x0 = x;
+				y0 = y;
+			}
+			break;
+		case GLUT_MIDDLE_BUTTON:
+			break;
+		case GLUT_RIGHT_BUTTON:
+			break;
+		default:
+			break;
+	}
+
 }
 
 void init(void){
@@ -33,6 +52,7 @@ int main(int argc, char *argv[]){
 	glutCreateWindow(argv[0]);
 	glutDisplayFunc(display);
 	glutReshapeFunc(resize);
+	glutMouseFunc(mouse);
 	init();
 	glutMainLoop();
 	return 0;
