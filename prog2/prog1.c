3d0fd40c07b763d623833e7da400751804cd8f72
arima0714 <a1710021@edu.cc.uec.ac.jp>
2019-10-15 11:34:50 +0900

    実行中のウィンドウサイズの変更に対応した

--- prog2/prog1.c
+++ prog2/prog1.c
@@ -1,7 +1,22 @@
+#include <stdio.h>
 #include <GL/glut.h>
 
+#define MAXPOINTS 100
+GLint point[MAXPOINTS][2];
+int pointnum = 0;
+
 void display(void){
+	int i;
+
 	glClear(GL_COLOR_BUFFER_BIT);
+	if(pointnum > 1){
+		glColor3d(0.0, 0.0, 0.0);
+		glBegin(GL_LINES);
+		for( i = 0; i < pointnum; ++i){
+			glVertex2iv(point[i]);
+		}
+		glEnd();
+	}
 	glFlush();
 }
 
@@ -12,23 +27,21 @@ void resize(int w, int h){
 }
 
 void mouse(int button, int state, int x, int y){
-	static int x0, y0;
-
 	switch(button){
 		case GLUT_LEFT_BUTTON:
+			point[pointnum][0] = x;
+			point[pointnum][1] = y;
 			if(state == GLUT_UP){
 				glColor3d(0.0, 0.0, 0.0);
 				glBegin(GL_LINES);
-				glVertex2i(x0, y0);
-				glVertex2i(x , y );
+				glVertex2iv(point[pointnum - 1]);	// ひとつ前は押した位置
+				glVertex2iv(point[pointnum]);		// 今の位置は話した位置
 				glEnd();
 				glFlush();
 			}
 			else{
-				// ボタンを押した位置を覚える
-				x0 = x;
-				y0 = y;
 			}
+			if(pointnum < MAXPOINTS - 1) ++pointnum;
 			break;
 		case GLUT_MIDDLE_BUTTON:
 			break;
